951ab5531d0e128dac6ba6216ca6ab5beff59e03
Hugo Escandon <hescandon6@gmail.com>
2018-07-11 15:40:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,9 @@
-def prime?(int)
-  (2..int -1).each do |i|
-    if (int % i) == 0
-      return false
-    else
-      return true
+def prime?(integer)
+    return false if integer < 2
+    (2..integer - 1).each do |x|
+        if (integer % x) == 0
+            return false
+        end
     end
-  
+    true
+end
